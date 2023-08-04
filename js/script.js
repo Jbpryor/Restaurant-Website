8526ea3877dfb8f119e5d41a8d58d21be1ef77d1
Jason Pryor <125523612+Jbpryor@users.noreply.github.com>
2023-08-03 23:04:29 -0500

    added the menu images and functionality

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ setInterval(showNextImage, 2500);
 
 const   body = document.querySelector('body'),
         header = document.querySelector('.header'),
+        homeSection = document.querySelector('.home'),
         navLinks = document.querySelectorAll('.nav-link'),
         navList = document.querySelector('.nav-list'),
         navContent = body.querySelector('.nav-content'),
@@ -37,9 +38,9 @@ const   body = document.querySelector('body'),
 
 
 navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
+    link.addEventListener('click', (event) => {
         navLinks.forEach(link => link.classList.remove('active'));
-        e.target.classList.add('active');
+        event.target.classList.add('active');
 
         if (navContent.classList.contains('open')) {
             navContent.classList.remove('open');
@@ -47,9 +48,9 @@ navLinks.forEach(link => {
         }
     });
 
-    link.addEventListener('mouseleave', (e) => {
-        if (!e.target.classList.contains('active')) {
-            e.target.classList.remove('active');
+    link.addEventListener('mouseleave', (event) => {
+        if (!event.target.classList.contains('active')) {
+            event.target.classList.remove('active');
         }        
     });
 });
@@ -77,8 +78,8 @@ window.addEventListener('resize', () => {
     updateBtnAndLogoDisplay();
 })
 
-document.addEventListener('click', (e) => {
-    if (!navContent.contains(e.target) && !navOpenBtn.contains(e.target)) {
+document.addEventListener('click', (event) => {
+    if (!navContent.contains(event.target) && !navOpenBtn.contains(event.target)) {
         navContent.classList.remove('open');
         updateBtnAndLogoDisplay();
     }
@@ -99,8 +100,8 @@ if (navContent && navOpenBtn) {
 
 function PageTransitions() {
     navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
 
             const targetSectionId = link.dataset.target;
             const targetSection = document.getElementById(targetSectionId);
@@ -109,6 +110,7 @@ function PageTransitions() {
                 section.classList.remove('active');
             });
             targetSection.classList.add('active');
+            updateNavBackground();
         });
     });
 };
@@ -116,8 +118,6 @@ function PageTransitions() {
 PageTransitions();
 
 // Overflow for home off
-
-const homeSection = document.querySelector('#home');
 
 function setBodyOverflow() {
         const isHomeActive = homeSection.classList.contains('active');
@@ -129,45 +129,117 @@ document.addEventListener('click', setBodyOverflow);
 
 // Sticky Nav Links
 
+let scrollHandler;
+
 function stickyNavLinks() {
     const headerHeight = header.clientHeight;
 
-if (window.innerWidth > 900) {
-    window.addEventListener('scroll', () => {
+    if (scrollHandler) {
+        window.removeEventListener('scroll', scrollHandler);
+    }
+
+    scrollHandler = () => {
         const scrollY = window.scrollY;
-    
-        if (scrollY >= headerHeight - 60) {
-            navList.classList.add('sticky');
-        } else {
-            navList.classList.remove('sticky');
-        }
-        if (scrollY >= 300) {
-            navList.classList.add('pad');
-        } else {
-            navList.classList.remove('pad');
-        }
-    });
-} else if (window.innerWidth <= 900 && window.innerWidth > 820){
-    window.addEventListener('scroll', () => {
-        const scrollY = window.scrollY;
-    
-        if (scrollY >= headerHeight + 140) {
-            navList.classList.add('sticky');
-        } else {
-            navList.classList.remove('sticky');
-        }
-    });
-} else if (window.innerWidth <= 820) {
-    navList.classList.remove('sticky');
-}
+
+
+        if (window.innerWidth > 900) {
+            if (scrollY >= headerHeight - 60) {
+                navList.classList.add('sticky');
+            } else {
+                navList.classList.remove('sticky');
+            }
+            if (scrollY >= 300) {
+                navList.classList.add('pad');
+            } else {
+                navList.classList.remove('pad');
+            }
+        } else if (window.innerWidth <= 900 && window.innerWidth > 820) {
+            if (scrollY >= headerHeight + 140) {
+                navList.classList.add('sticky');
+            } else {
+                navList.classList.remove('sticky');
+            }
+            if (scrollY >= 300) {
+                navList.classList.add('pad');
+            } else {
+                navList.classList.remove('pad');
+            }
+        } else if (window.innerWidth <= 820) {
+                navList.classList.remove('sticky');
+                if (!navContent.classList.contains('open')) {
+                    navList.classList.add('pad');
+                    navList.classList.add('flex'); 
+                }
+
+        };
+    };
+    window.addEventListener('scroll', scrollHandler);
 };
 
 stickyNavLinks();
 
-// window.addEventListener('resize', () => {
-//     updateBtnAndLogoDisplay();
-//     stickyNavLinks();
-// })
+window.addEventListener('resize', () => {
+    updateBtnAndLogoDisplay();
+    stickyNavLinks;
+});
 
 
-// trying to figure out why the nav bar gets triggered on resize at < 820px
+// Remove 'open' class
+
+function removeOpenClass() {
+    if (navContent.classList.contains('open')) {
+        navContent.classList.remove('open');
+        updateBtnAndLogoDisplay;
+        stickyNavLinks();
+    }
+};
+
+// change background for othe pages than main
+
+function hasClass(element, className) {
+    return element.classList.contains(className);
+};
+
+function updateNavBackground() {
+    const screenWidth = window.innerWidth;
+    const activeSection = document.querySelector('.section.active');
+
+    if (screenWidth <= 500 && activeSection !== homeSection || (screenWidth > 500 && screenWidth <= 820)) {
+        navContent.style.backgroundColor = 'var(--black-color)';
+    } else if (screenWidth <= 500 && activeSection === homeSection) {
+        navContent.style.backgroundColor = 'transparent';
+    } else if (screenWidth > 820) {
+        navContent.style.backgroundColor = null;
+    }
+};
+
+window.addEventListener('DOMContentLoaded', updateNavBackground);
+window.addEventListener('resize', updateNavBackground);
+
+
+// Function to open menu img in full screen
+
+function openFullScreenMenu(event) {
+    const clickedMenu = event.target;
+
+    const fullScreenDiv = document.createElement('div')
+    fullScreenDiv.classList.add('full-screen-menu');
+
+    const fullScreenMenu = new Image();
+    fullScreenMenu.src = clickedMenu.src;
+    fullScreenMenu.classList.add('full-screen-image');
+
+    fullScreenDiv.appendChild(fullScreenMenu);
+
+    document.body.appendChild(fullScreenDiv);
+
+    fullScreenDiv.addEventListener('click', () => {
+        fullScreenDiv.remove();
+    });
+}
+
+const fullScreenMenus = document.querySelectorAll('.full-screen-img');
+
+fullScreenMenus.forEach(menu => {
+    menu.addEventListener('click', openFullScreenMenu);
+});
